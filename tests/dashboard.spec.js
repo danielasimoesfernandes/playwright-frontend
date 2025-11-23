4e227e2e2c1c57224836c05508faaef2c1487ae0
Daniela Fernandes <104898358+danielasimoesfernandes@users.noreply.github.com>
2025-11-23 15:42:18 +0000

    Chanegs

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -22,3 +22,24 @@ test('CT-FE-005: Verificar Proteção de Rotas', async ({ page }) => {
   await loginPage.validarPaginaLogin();
   
 });
+
+//////////////////////////////////////////////////////////
+
+test('CT-FE-006: Visualizar Dashboard com Estatísticas', async ({ page }) => {
+
+  const loginPage = new LoginPage(page);
+  const dashboardPage = new DashboardPage(page);
+
+  // Abrir pagina login (qualquer pagina, so para limpar o storage)
+  await loginPage.goToWebsite(); 
+
+  // Limpar storage
+  await page.evaluate(() => localStorage.clear());
+
+  // Ir para a página de dashboard sem fazer login
+  await dashboardPage.goToDashboard();
+
+  // Validar redirecinamento para Login page 
+  await loginPage.validarPaginaLogin();
+  
+});
